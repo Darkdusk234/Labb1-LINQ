728cc9fa2b194d9397a8fadfe56e75220aa3d667
darkdusk234 <johan.svensson.01@hotmail.com>
2025-04-07 15:23:25 +0200

    Added function to get all products in a category ordered by price descending

--- Labb1-LINQ/Program.cs
+++ Labb1-LINQ/Program.cs
@@ -8,5 +8,31 @@ namespace Labb1_LINQ
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public void GetProductsByCategoryOrderPrice(string category)
+        {
+            using (var context = new InternetShopContext())
+            {
+                try
+                {
+                    var products = context.Products
+                        .Where(p => p.Category.Name == category)
+                        .Select(p => new { p.Name, p.Description, p.Price, p.StockQuantity })
+                        .OrderByDescending(p => p.Price);
+
+                    Console.WriteLine("| Namn |  Beskrivning |  Pris |  Lagermängd |");
+
+                    foreach (var product in products)
+                    {
+                        Console.WriteLine($"| {product.Name} |  {product.Description} |  {product.Price} |  {product.StockQuantity} |");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
     }
 }
