0b2d57f2d13592d40dfd4cbb5651bb75d47a1dd6
darkdusk234 <johan.svensson.01@hotmail.com>
2025-04-08 09:43:31 +0200

    Added a menu where user can choose what method to test. Also added a few lines of code to make it look good in console.

--- Labb1-LINQ/Program.cs
+++ Labb1-LINQ/Program.cs
@@ -7,8 +7,40 @@ namespace Labb1_LINQ
     {
         static void Main(string[] args)
         {
-            //GetProductsByCategoryPriceOrder("Electronics");
-            GetSupplierWithLowStockAmount();
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            bool loopActive = true;
+            while(loopActive)
+            {
+                Console.Clear();
+                Console.WriteLine("Skriv nummret av den metod du vill testa.");
+                Console.WriteLine("1. Hämta alla produkter i Elektronics kategorin sorterat efter största pris.");
+                Console.WriteLine("2. Hämta alla levrantörer som har produkter där lagersaldot är under tio.");
+                Console.WriteLine("3. Avsluta programmet.");
+
+                int choice = 0;
+
+                while (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Skriv endast siffror");
+                }
+
+                switch(choice)
+                {
+                    case 1:
+                        GetProductsByCategoryPriceOrder("Electronics");
+                        break;
+                    case 2:
+                        GetSupplierWithLowStockAmount();
+                        break;
+                    case 3:
+                        loopActive = false;
+                        break;
+                }
+            }
         }
 
         public static void GetProductsByCategoryPriceOrder(string category)
@@ -24,12 +56,17 @@ namespace Labb1_LINQ
 
                     if (products != null)
                     {
+                        Console.Clear();
                         Console.WriteLine("| Namn | Beskrivning | Pris | Lagermängd |");
 
                         foreach (var product in products)
                         {
                             Console.WriteLine($"| {product.Name} |  {(product.Description != null ? product.Description : "N/A")} |  {product.Price} |  {product.StockQuantity} |");
                         }
+
+                        Console.WriteLine();
+                        Console.WriteLine("Tryck enter för att gå tillbaka till menyn.");
+                        Console.ReadLine();
                     }
                 }
                 catch (Exception ex)
@@ -47,16 +84,24 @@ namespace Labb1_LINQ
                     var suppliers = context.Products
                         .Include(p => p.Supplier)
                         .Where(p => p.StockQuantity < 10)
-                        .Select(s => new { s.Supplier.Name, s.Supplier.ContactPerson, s.Supplier.Email, s.Supplier.Phone });
+                        .Select(s => new { s.Supplier.Name, s.Supplier.ContactPerson, s.Supplier.Email, s.Supplier.Phone })
+                        .ToList();
 
                     if (suppliers != null)
                     {
+                        suppliers = suppliers.Distinct().ToList();
+
+                        Console.Clear();
                         Console.WriteLine("| Företags Namn |  Kontaktperson | Email | Telefonnummer |");
 
                         foreach (var supplier in suppliers)
                         {
                             Console.WriteLine($"| {supplier.Name} | {supplier.ContactPerson} | {(supplier.Email != null ? supplier.Email : "N/A")} | {(supplier.Phone != null ? supplier.Phone : "N/A")} |");
                         }
+
+                        Console.WriteLine();
+                        Console.WriteLine("Tryck enter för att gå tillbaka till menyn.");
+                        Console.ReadLine();
                     }
                 }
                 catch (Exception ex)
